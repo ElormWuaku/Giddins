674f014b8074b240c5546b013373b3157995ee6a
ElormWuaku <hageelorm@gmail.com>
2024-06-14 12:15:37 +0000

    Final work on Giddins

--- JS/index.js
+++ JS/index.js
@@ -12,3 +12,16 @@ function showSlides() {
     slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
+
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdown')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i <dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
